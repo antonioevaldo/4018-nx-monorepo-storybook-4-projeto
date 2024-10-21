0d2e89c56ad1488b06fd74b48002916611a56bcf
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-10-21 11:18:37 -0300

    feat: cria componente typography e stories

---
+++ libs/storybook-host/src/lib/Atoms/typography.component.stories.ts
@@ -0,0 +1,102 @@
+import { argsToTemplate, type Meta, type StoryObj } from '@storybook/angular';
+import { TypographyComponent } from '@alfabit-alura-teste/typography';
+
+const meta: Meta<TypographyComponent> = {
+  component: TypographyComponent,
+  render: (args) => ({
+    props: args,
+    template: `
+      <ab-typography ${argsToTemplate(args)}>
+        Text
+      </ab-typography>
+    `,
+  }),
+};
+
+export default meta;
+
+type Story = StoryObj<TypographyComponent>;
+
+export const Default: Story = {};
+
+export const Title1: Story = {
+  args: {
+    type: 'title1',
+  },
+};
+
+export const Title2: Story = {
+  args: {
+    type: 'title2',
+  },
+};
+
+export const Title3: Story = {
+  args: {
+    type: 'title3',
+  },
+};
+
+export const Subtitle1: Story = {
+  args: {
+    type: 'subtitle1',
+  },
+};
+
+export const Subtitle2: Story = {
+  args: {
+    type: 'subtitle2',
+  },
+};
+
+export const TextLarge: Story = {
+  args: {
+    type: 'normal',
+    size: 'lg',
+  },
+};
+
+export const TextMedium: Story = {
+  args: {
+    ...TextLarge.args,
+    size: 'md',
+  },
+};
+
+export const TextSmall: Story = {
+  args: {
+    ...TextLarge.args,
+    size: 'sm',
+  },
+};
+
+export const TextExtraSmall: Story = {
+  args: {
+    ...TextLarge.args,
+    size: 'xs',
+  },
+};
+
+export const Title2WithMediumSize: Story = {
+  args: {
+    ...Title2.args,
+    size: 'md',
+  },
+};
+
+export const Paragraph: Story = {
+  args: {
+    type: 'normal',
+    size: 'lg',
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+      <p>
+        <ab-typography ${argsToTemplate(args)}>
+          Primeiro <strong>parágrafo</strong>
+        </ab-typography>
+      </p>
+    `,
+  }),
+};
--- libs/ui/typography/src/lib/typography/typography.component.ts
+++ libs/ui/typography/src/lib/typography/typography.component.ts
@@ -1,11 +1,70 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  Directive,
+  Input,
+  TemplateRef,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+@Directive({
+  selector: '[abContainerContent]',
+  standalone: true,
+})
+export class ContainerContentDirective {
+  constructor(public templateRef: TemplateRef<unknown>) {}
+}
+
+type Text =
+  | 'title1'
+  | 'title2'
+  | 'title3'
+  | 'subtitle1'
+  | 'subtitle2'
+  | 'normal';
+
+type Size =
+  | 'title1'
+  | 'title2'
+  | 'title3'
+  | 'subtitle1'
+  | 'subtitle2'
+  | 'lg'
+  | 'md'
+  | 'sm'
+  | 'xs';
+
 @Component({
-  selector: 'ab-typography',
+  selector: 'ab-inner-typography',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './typography.component.html',
   styleUrl: './typography.component.css',
 })
-export class TypographyComponent {}
+export class InnerTypographyComponent {
+  @Input() type!: Text;
+  @Input() size!: Size;
+
+  @ContentChild(ContainerContentDirective) content!: ContainerContentDirective;
+
+  getClasses(): string {
+    return `typography ${this.size}`;
+  }
+}
+
+@Component({
+  selector: 'ab-typography',
+  standalone: true,
+  imports: [CommonModule, InnerTypographyComponent, ContainerContentDirective],
+  template: `
+    <ab-inner-typography [type]="type" [size]="size">
+      <ng-template abContainerContent>
+        <ng-content></ng-content>
+      </ng-template>
+    </ab-inner-typography>
+  `,
+})
+export class TypographyComponent {
+  @Input() type: Text = 'normal';
+  @Input() size!: Size;
+}
